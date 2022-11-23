6e8a37daeb4b2a1337aa4ec384815189e68d7486
Kevin Muchene <muchenekevin@gmail.com>
2022-11-23 11:47:14 -0600

    Deleted logo import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import PageRoutes from "./routes/PageRoutes";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <PageRoutes/>
+        <PageRoutes />
       </header>
     </div>
   );
