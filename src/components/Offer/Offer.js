1e41f88134114d3017674deec36dbd63cbd26173
Khadher Alqiari <k.alqiari@floranow.com>
2022-11-25 15:35:09 -0600

    Update Offer.js

--- src/components/Offer/Offer.js
+++ src/components/Offer/Offer.js
@@ -1,7 +1,9 @@
-import React from "react";
-import { Col, Container, Row, Card } from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+import {Col, Container, Row, Card, ProgressBar} from "react-bootstrap";
 import houseImage from "../../resources/images/house.jpg";
 import Button from "react-bootstrap/Button";
+import {useKeycloak} from "@react-keycloak/web";
+import Api from "../../utils/api";
 // {
 //     "propertyId": 22,
 //     "applicationType": null,
@@ -48,40 +50,83 @@ import Button from "react-bootstrap/Button";
 //     "id": 1
 // }
 function Offer(props) {
+    const {keycloak} = useKeycloak();
+    const [status, setStatus] = useState();
+    const [flag, setFlag] = useState(false);
+    function AcceptOffer() {
+        Api.put("/api/v1/applications/" + props.app.id + '/accept', {}, {
+            headers: keycloak?.token ? {authorization: `Bearer ${keycloak?.token}`} : {}
+        }).then(response => {
+            props.app.reviewStatus = 'Accepted';
+            setFlag(!flag)
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
+    function RejectOffer() {
+        Api.put("/api/v1/applications/" + props.app.id + '/reject', {}, {
+            headers: keycloak?.token ? {authorization: `Bearer ${keycloak?.token}`} : {}
+        }).then(response => {
+            props.app.reviewStatus = 'Rejected';
+            setFlag(!flag)
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
+    useEffect(()=> {
+        setStatus(props.app.reviewStatus == "Pending" ? <Row>
+
+            <Col>
+                <Button style={{width: "100%"}} variant="success" onClick={AcceptOffer}>
+                    Accept
+                </Button>
+            </Col>
+            <Col>
+                <Button style={{width: "100%"}} variant="danger" onClick={RejectOffer}>
+                    Decline
+                </Button>
+            </Col>
+        </Row> : props.app.reviewStatus == "Accepted" ?<Col>
+                <div>
+                    Status: {props.app.reviewStatus}{" "}
+                    <span><ProgressBar variant="success" now={100} /></span>
+                </div>
+            </Col>
+            : <Col>
+                <div>
+                    Status: {props.app.reviewStatus}{" "}
+                    <span><ProgressBar variant="danger" now={100} /></span>
+                </div>
+            </Col>)
+    }, [flag])
+
+
     return (
         <div>
             <Container>
                 <Row>
                     <Col className="mb-4">
-                        <Card style={{ width: "18rem" }}>
-                            <Card.Img width={280} height={210} variant="top" src={props.app.property.photos[0].url} />
+                        <Card style={{width: "18rem"}}>
+                            <Card.Img width={280} height={210} variant="top" src={props.app.property.photos[0].url}/>
                             <Card.Body>
-                                <Card.Title>Customer Name: {props.app.user.firstName + ' ' + props.app.user.firstName} </Card.Title>
+                                <Card.Title>Customer
+                                    Name: {props.app.user.firstName + ' ' + props.app.user.firstName} </Card.Title>
                                 <Card.Text>
                                     {" "}
-                                    <b>Application Type:</b>Rent
+                                    <b>Application Type:</b>{props.app.applicationType}
                                 </Card.Text>
                                 <Card.Text>
-                                    <b>Employment Info:</b> Criminal
+                                    <b>Employment Info:</b> {props.app.employmentInfo}
                                 </Card.Text>
                                 <Card.Text>
-                                    <b>Credit Score:</b> 7
+                                    <b>Credit Score:</b> {props.app.creditScore}
                                 </Card.Text>
                                 <Card.Text>
-                                    <b>Message:</b> I'll be paying it timely with stolen money
+                                    <b>Message:</b> {props.app.message}
                                 </Card.Text>
-                                <Row>
-                                    <Col>
-                                        <Button style={{ width: "100%" }} variant="success">
-                                            Accept
-                                        </Button>
-                                    </Col>
-                                    <Col>
-                                        <Button style={{ width: "100%" }} variant="danger">
-                                            Decline
-                                        </Button>
-                                    </Col>
-                                </Row>
+                                {status}
                             </Card.Body>
                         </Card>
                     </Col>
